acb8372f2ad3b588037d910e3358d9ec8c90ac97
Ilya <52508362+Ori-pyt@users.noreply.github.com>
2019-07-07 16:20:49 +0300

    Changes

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ print('Hello world\n')
 age = 26
 name = 'Swaroop'
 name2 = 'Tom'
-print('Возраст {1} -- {0} лет.'.format(age, name))
+print('Age {1} -- {0} year.'.format(age, name))
 print('Почему {0} забавляется с этим Python?'.format(name)+' ,а {1} на это смотрит'.format(name, name2))
 
 
