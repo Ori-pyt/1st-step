196f0e7757ba51ed6bf8e6682b420de289900710
Ilya <52508362+Ori-pyt@users.noreply.github.com>
2019-07-07 23:09:32 +0300

    Name change

--- practice.py
+++ practice.py
@@ -8,7 +8,7 @@ print(s)
 '''
 
 # Calculator with two variables
-number_1 = int(input('Enter your number(a): '))
+'''number_1 = int(input('Enter your number(a): '))
 number_2 = int(input('Enter your number(b): '))
 print('What do you wanna do with this numbers?( p-plus | m-minus | mul-multiplication | d-division )')
 action = str(input('Enter your number: '))
@@ -23,4 +23,22 @@ elif action == 'd':
 else:
     print('No such action')
 
-print('end')
+print('end')'''
+
+
+# Area of ​​a right triangle
+'''h = int(input('High:'))
+b = int(input('Base:'))
+a = 1/2*(h*b)
+print('Area = ', a)
+
+n = int(input('School:'))
+k = int(input('Apple:'))
+if n > k:
+    print(k/n)
+elif k >n:
+    print('for schoolchildren ', k//n)
+    print('apples in the basket ', k%n)
+else:
+    print(1)'''
+
