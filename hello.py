14d9ddad8e137efb3d872cdb42a03c0c624edc51
Ilya <52508362+Ori-pyt@users.noreply.github.com>
2019-07-07 15:42:08 +0300

    Add 1st project

---
+++ hello.py
@@ -0,0 +1,12 @@
+# just hello)
+print('Hello world\n')
+
+
+# what is <format()>?
+age = 26
+name = 'Swaroop'
+name2 = 'Tom'
+print('Возраст {1} -- {0} лет.'.format(age, name))
+print('Почему {0} забавляется с этим Python?'.format(name)+' ,а {1} на это смотрит'.format(name, name2))
+
+
